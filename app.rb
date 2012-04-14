8ad30adf91f44609babc338f894cadb4ae06cd3c
Kevin Prince <nextgengames@gmail.com>
2012-04-14 17:02:48 +0100

    tweaked copy

--- app.rb
+++ app.rb
@@ -7,6 +7,10 @@ get '/' do
   erb :index
 end
 
+post '/findme' do
+  #see if we have the user on record and if we do fwd them to update email
+end
+
 get '/update/:email' do
  #Show subscription settings for the email address
 end
